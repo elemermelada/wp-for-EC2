cc1a7a6bed9f8a801fb205d3e1c7e0924905e41e
elemermelada <iiasanmi9@gmail.com>
2024-11-27 11:36:10 +0100

    fix: start db before attempting backup

--- backup.sh
+++ backup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+sudo systemctl start mariadb.service
+
 backuppath="backups/$(date | sed 's/ //g' | sed 's/://g')"
 mkdir -p "$backuppath"
 
 cp -R /var/www/html "$backuppath/html"
-sudo mariadb-dump -x -A > "$backuppath/db.sql"
+
+until sudo mariadb-dump -x -A > "$backuppath/db.sql"
+do
+    echo "Trying to connect to db..."
+    sleep 1
+done
